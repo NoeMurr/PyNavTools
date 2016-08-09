61d76678dad0d3ca61cf08cb2f0ac40be50ade1f
Noè Murr <noe.murr@outlook.com>
2016-08-09 23:31:55 +0200

    Added Modules

---
+++ __init__.py
@@ -0,0 +1,17 @@
+##############################################################################
+#
+# @author:  Noè Murr
+# @file:    __init__.py
+# @brief:   simple init file for PyNavTool module.
+#
+##############################################################################
+
+# necessary modules.
+
+# math is necessary for navigation calculation.
+import math #@UnusedImport
+
+import RhumbLine
+
+
+
